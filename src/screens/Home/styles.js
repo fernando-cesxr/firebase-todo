71e7763c56d615ae768dad3db925bf275362b8be
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-05 09:47:39 -0300

    ta tudo errado provavelmente

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,69 +1,54 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components'
 
 export const Container = styled.View`
   flex: 1;
   padding: 20px;
   margin: 20px 0 0 0;
-  background-color: #FFF;
+  background-color: #d3d3d3;
 `;
 
+
 export const Body = styled.View`
-  flex: 1;
+flex: 1;
 `;
 
 export const List = styled.FlatList`
-  flex: 1;
-  margin: 5px 0 0 0;
+flex: 1;
+margin: 5px 0 0 0;
 `;
 
 export const ContainerList = styled.View`
-  margin-bottom: 15px;
+  margin: 0 0 5px 0;
   padding: 15px;
   border-radius: 4px;
-  background-color: #eee;
-  display: flex;
+  background-color: white;
   flex-direction: row;
+  border: 1px solid #000;
   justify-content: space-between;
-  border: 1px solid #eee;
 `;
 
 export const Text = styled.Text`
-  font-size: 14px;
-  color: #333;
-  font-weight: bold;
-  margin: 4px 0 0 0;
-  text-align: center;
+font-size: 14px;
+font-weight: bold;
+margin: 4px 0 0 0;
+text-align: center;
 `;
 
-export const Icon = styled.TouchableOpacity``;
+export const Icon = styled.TouchableOpacity`
+
+`;
 
 export const Form = styled.View`
-  padding: 0;
-  height: 60px;
-  justify-content: center;
-  align-self: stretch;
-  flex-direction: row;
-  padding-top: 13px;
-  border-top-width: 1px;
-  border-color: #eee;
+padding: 0;
+height: 60px;
+justify-content: center;
+align-self: stretch;
+flex-direction: row;
+padding: 13px 0 0 0;
+border-top-width: 1px;
+border-color: #000;
 `;
 
-export const Input = styled.TextInput`
-  flex: 1;
-  height: 40px;
-  background-color: #eee;
-  border-radius: 4px;
-  padding: 10px 5px;
-  border-width: 1px;
-  border-color: #eee;
-`;
+export const InputTask = styled.textInput`
 
-export const Button = styled.TouchableOpacity`
-  height: 40px;
-  width: 40px;
-  justify-content: center;
-  align-items: center;
-  background-color: #1c6cce;
-  border-radius: 4px;
-  margin: 0 0 0 10px;
 `;
